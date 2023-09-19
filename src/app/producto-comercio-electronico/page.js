e603f93bb8a87645c15551d96946cfb3e7484017
catharygr <103951447+catharygr@users.noreply.github.com>
2023-09-19 17:37:49 +0100

    Trabajando con CSS

--- src/app/producto-comercio-electronico/page.js
+++ src/app/producto-comercio-electronico/page.js
@@ -2,40 +2,46 @@
 import styles from "./nav.module.css";
 import { motion } from "framer-motion";
 import Link from "next/link";
+import Image from "next/image";
+import logo from "@/app/image/logo.svg";
 import { useState, useId } from "react";
 
 export default function Nav() {
   const [hoveredNavItem, setHoveredNavItem] = useState(null);
   const id = useId();
   return (
-    <nav
-      className={styles.container}
-      onMouseLeave={() => setHoveredNavItem(null)}
-    >
-      <ul className={styles.ul}>
-        {data.map(({ slug, text, url }) => (
-          <li key={slug}>
-            {hoveredNavItem === slug && (
-              <motion.div
-                layoutId={id}
-                className={styles.fondo}
-                initial={{
-                  borderRadius: 10,
-                }}
-              />
-            )}
+    <div className={styles.logo}>
+      <nav
+        className={styles.container}
+        onMouseLeave={() => setHoveredNavItem(null)}
+      >
+        <Image className={styles.image} src={logo} alt="Logo de la empresa" />
 
-            <Link
-              className={styles.a}
-              href={url}
-              onMouseEnter={() => setHoveredNavItem(slug)}
-            >
-              {text}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </nav>
+        <ul className={styles.ul}>
+          {data.map(({ slug, text, url }) => (
+            <li key={slug}>
+              {hoveredNavItem === slug && (
+                <motion.div
+                  layoutId={id}
+                  className={styles.fondo}
+                  initial={{
+                    borderRadius: 10,
+                  }}
+                />
+              )}
+
+              <Link
+                className={styles.a}
+                href={url}
+                onMouseEnter={() => setHoveredNavItem(slug)}
+              >
+                {text}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </div>
   );
 }
 
