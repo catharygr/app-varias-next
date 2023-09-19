a98be108cacda737ffb3f838028085fcb5c4caf1
catharygr <103951447+catharygr@users.noreply.github.com>
2023-09-19 16:07:44 +0100

    Trabajando en la página Producto

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <Link className={styles.about} href="/producto-comercio-electronico">
+        Producto de Comercio Electrónico
+      </Link>
+    </main>
+  );
 }
---
+++ src/app/producto-comercio-electronico/page.js
@@ -0,0 +1,9 @@
+import styles from "./producto.module.css";
+
+export default function Producto() {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Producto de Comercio Electrónico</h1>
+    </div>
+  );
+}
