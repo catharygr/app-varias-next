074f0a27ac0faed85fd8d31dee3e6bb9f11aed33
catharygr <103951447+catharygr@users.noreply.github.com>
2023-09-20 16:46:18 +0100

    Trabajando con las rutas

--- src/app/producto-comercio-electronico/page.js
+++ src/app/producto-comercio-electronico/page.js
@@ -1,76 +1,10 @@
-"use client";
+import Nav from "./componentes/nav";
 import styles from "./nav.module.css";
-import { motion } from "framer-motion";
-import Link from "next/link";
-import Image from "next/image";
-import logo from "./image/logo.svg";
-import icon from "./image/icon-cart.svg";
-import { useState, useId } from "react";
 
-export default function Nav() {
-  const [hoveredNavItem, setHoveredNavItem] = useState(null);
-  const id = useId();
+export default function Home() {
   return (
-    <nav
-      className={styles.container}
-      onMouseLeave={() => setHoveredNavItem(null)}
-    >
-      <div className={styles.image}>
-        <Image src={logo} alt="Logo de la empresa" />
-      </div>
-
-      <ul className={styles.ul}>
-        {data.map(({ slug, text, url }) => (
-          <li key={slug}>
-            {hoveredNavItem === slug && (
-              <motion.div
-                layoutId={id}
-                className={styles.fondo}
-                initial={{
-                  borderRadius: 10,
-                }}
-              />
-            )}
-
-            <Link
-              className={styles.a}
-              href={url}
-              onMouseEnter={() => setHoveredNavItem(slug)}
-            >
-              {text}
-            </Link>
-          </li>
-        ))}
-      </ul>
-      <Image className={styles.icon} src={icon} alt="Icono de carrito" />
-    </nav>
+    <div className={styles.container}>
+      <Nav />
+    </div>
   );
 }
-
-const data = [
-  {
-    slug: "collections",
-    text: "Collections",
-    url: "/producto-comercio-electronico/collections",
-  },
-  {
-    slug: "men",
-    text: "Men",
-    url: "/producto-comercio-electronico/men",
-  },
-  {
-    slug: "women",
-    text: "Women",
-    url: "/producto-comercio-electronico/women",
-  },
-  {
-    slug: "about",
-    text: "About",
-    url: "/producto-comercio-electronico/about",
-  },
-  {
-    slug: "contact",
-    text: "Contact",
-    url: "/producto-comercio-electronico/contact",
-  },
-];
