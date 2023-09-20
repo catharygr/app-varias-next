abdc278dcc9646fc78b6a2203fa4453b36d19dc6
catharygr <103951447+catharygr@users.noreply.github.com>
2023-09-20 18:33:36 +0100

    Trabajando con la lógica de la app

--- src/app/producto-comercio-electronico/collections/page.js
+++ src/app/producto-comercio-electronico/collections/page.js
@@ -1,19 +1,44 @@
 '"use client";';
 import styles from "./collections.module.css";
 import Image from "next/image";
-import product1 from "../image/image-product-4.jpg";
+import product1 from "../image/image-product-1-thumbnail.jpg";
+import product1A from "../image/image-product-1.jpg";
+import product2 from "../image/image-product-2-thumbnail.jpg";
+import product2A from "../image/image-product-2.jpg";
+import product3 from "../image/image-product-3-thumbnail.jpg";
+import product3A from "../image/image-product-3.jpg";
+import product4 from "../image/image-product-4-thumbnail.jpg";
+import product4A from "../image/image-product-4.jpg";
+import iconPlus from "../image/icon-plus.svg";
+import iconMinus from "../image/icon-minus.svg";
+import iconClose from "../image/icon-close.svg";
+import iconDelete from "../image/icon-delete.svg";
+import iconMenu from "../image/icon-menu.svg";
 
 export default function Collections() {
   return (
     <div className={styles.container}>
-      <h3>Sneaker Company</h3>
-      <h2>Fall Limited Edition Sneakers</h2>
-      {data.map(({ id, image, name, price, slug }) => (
+      {data.map(({ id, name, image, price, slug }) => (
         <div key={id} className={styles.producto}>
           <Image src={product1} alt={name} />
+          <Image src={product1A} alt={name} />
+          <Image src={product2} alt={name} />
+          <Image src={product2A} alt={name} />
+          <Image src={product3} alt={name} />
+          <Image src={product3A} alt={name} />
+          <Image src={product4} alt={name} />
+          <Image src={product4A} alt={name} />
+          <Image src={iconPlus} alt={name} />
+          <Image src={iconMinus} alt={name} />
+          <Image src={iconClose} alt={name} />
+          <Image src={iconDelete} alt={name} />
+          <Image src={iconMenu} alt={name} />
+
+          <h3>Sneaker Company</h3>
+          <h2>Fall Limited Edition Sneakers</h2>
           <h3>{name}</h3>
           <p>${price}</p>
-          <a href={`/collections/${slug}`}>Ver producto</a>
+          <p>{slug}</p>
         </div>
       ))}
     </div>
