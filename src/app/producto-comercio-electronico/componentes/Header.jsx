cf1b8212678678f3be9f1b5fb175cd8de616fdc1
catharygr <103951447+catharygr@users.noreply.github.com>
2023-09-21 16:55:28 +0100

    Trabajando con el Menú

--- src/app/producto-comercio-electronico/componentes/Header.jsx
+++ src/app/producto-comercio-electronico/componentes/Header.jsx
@@ -1,22 +1,25 @@
+"use client";
 import Image from "next/image";
 import logo from "../image/logo.svg";
 import icon from "../image/icon-cart.svg";
 import styles from "./nav.module.css";
 import Nav from "./Nav";
 import { Menu } from "react-feather";
+import { useState } from "react";
 
 export default function Header() {
+  const [estaMenuAbierto, setEstaMenuAbierto] = useState(false);
   return (
     <header className={styles.container}>
       <div>
-        <button>
+        <button onClick={setEstaMenuAbierto}>
           <Menu className={styles.hamburgerBtn} />
+          {estaMenuAbierto && <Nav />}
         </button>
       </div>
       <div className={styles.logo}>
         <Image src={logo} alt="Logo de la empresa" />
       </div>
-      <Nav />
       <Image className={styles.icon} src={icon} alt="Icono de carrito" />
     </header>
   );
