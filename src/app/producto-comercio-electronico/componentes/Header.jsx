3a49b8dc8103a27a637449da699aa1e3c9543f30
catharygr <103951447+catharygr@users.noreply.github.com>
2023-09-21 14:17:25 +0100

    Trabajando con react-feather

--- src/app/producto-comercio-electronico/componentes/Header.jsx
+++ src/app/producto-comercio-electronico/componentes/Header.jsx
@@ -3,10 +3,14 @@ import logo from "../image/logo.svg";
 import icon from "../image/icon-cart.svg";
 import styles from "./nav.module.css";
 import Nav from "./Nav";
+import { Menu } from "react-feather";
 
 export default function Header() {
   return (
     <header className={styles.container}>
+      <div>
+        <button>Hola</button>
+      </div>
       <div className={styles.logo}>
         <Image src={logo} alt="Logo de la empresa" />
       </div>
