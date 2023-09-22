40170f64bc94504997023e657e521ac4a6fbd67d
catharygr <103951447+catharygr@users.noreply.github.com>
2023-09-22 13:22:49 +0100

    Trabajando con media query para el móvil

--- src/app/producto-comercio-electronico/componentes/Header.jsx
+++ src/app/producto-comercio-electronico/componentes/Header.jsx
@@ -5,30 +5,36 @@ import icon from "../image/icon-cart.svg";
 import styles from "./nav.module.css";
 import Nav from "./Nav";
 import { Menu } from "react-feather";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Drawer from "./Drawer";
 import VisuallyHidden from "./VisuallyHidden";
 import useToggle from "./use-toggle";
 
 export default function Header() {
   const [estaMenuAbierto, setEstaMenuAbierto] = useToggle(false);
+  const [esMovil, setEsMovil] = useState(false);
+
   return (
     <header className={styles.container}>
-      <div>
-        <button
-          onClick={setEstaMenuAbierto}
-          className={styles.hamburgerBtn}
-          aria-expanded={estaMenuAbierto}
-        >
-          <Menu aria-hidden="true" focusable="false" />
-          <VisuallyHidden>Menu</VisuallyHidden>
-        </button>
-        {estaMenuAbierto && (
-          <Drawer handleDismiss={setEstaMenuAbierto}>
-            <Nav className={styles.navigationList} />
-          </Drawer>
-        )}
-      </div>
+      {esMovil ? (
+        <div>
+          <button
+            onClick={setEstaMenuAbierto}
+            className={styles.hamburgerBtn}
+            aria-expanded={estaMenuAbierto}
+          >
+            <Menu aria-hidden="true" focusable="false" />
+            <VisuallyHidden>Menu</VisuallyHidden>
+          </button>
+          {estaMenuAbierto && (
+            <Drawer handleDismiss={setEstaMenuAbierto}>
+              <Nav className={styles.navigationList} />
+            </Drawer>
+          )}
+        </div>
+      ) : (
+        <Nav className={styles.navigationList} />
+      )}
       <div className={styles.logo}>
         <Image src={logo} alt="Logo de la empresa" />
       </div>
