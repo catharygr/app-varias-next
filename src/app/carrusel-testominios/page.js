f6b561498bc865907dab57775c75a3d8f6ef548d
catharygr <103951447+catharygr@users.noreply.github.com>
2023-09-23 12:13:34 +0100

    Trabajando con la página carrusel y CSS

--- src/app/carrusel-testominios/page.js
+++ src/app/carrusel-testominios/page.js
@@ -1,12 +1,24 @@
+import styles from "./carrusel.module.css";
+
 export default function Carrusel() {
   return (
-    <div className="container">
-      <p>
-        {" "}
-        I amve been interested in coding for a while but never taken the jump,
-        until now. I could not recommend this course enough. I am now in the job
-        of my dreams and so excited about the future.{" "}
-      </p>
+    <div className={styles.container}>
+      <div className={styles.carruselIzq}>
+        <p>
+          {" "}
+          I amve been interested in coding for a while but never taken the jump,
+          until now. I could not recommend this course enough. I am now in the
+          job of my dreams and so excited about the future.{" "}
+        </p>
+      </div>
+      <div className={styles.carruselDer}>
+        <p>
+          {" "}
+          I amve been interested in coding for a while but never taken the jump,
+          until now. I could not recommend this course enough. I am now in the
+          job of my dreams and so excited about the future.{" "}
+        </p>
+      </div>
     </div>
   );
 }
