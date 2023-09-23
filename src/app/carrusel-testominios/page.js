23ef2ddadfdcdee2a32063af1b38f70d355d9882
catharygr <103951447+catharygr@users.noreply.github.com>
2023-09-23 13:10:26 +0100

    Trabajando con la página carrusel y CSS

--- src/app/carrusel-testominios/page.js
+++ src/app/carrusel-testominios/page.js
@@ -1,23 +1,29 @@
 import styles from "./carrusel.module.css";
+import Image from "next/image";
+import bg from "@/app/carrusel-testominios/images/pattern-bg.svg";
+import tanya from "@/app/carrusel-testominios/images/image-tanya.jpg";
+import quotes from "@/app/carrusel-testominios/images/pattern-quotes.svg";
+import curve from "@/app/carrusel-testominios/images/pattern-curve.svg";
 
 export default function Carrusel() {
   return (
     <div className={styles.container}>
-      <div className={styles.carruselIzq}>
-        <p>
-          {" "}
-          I amve been interested in coding for a while but never taken the jump,
-          until now. I could not recommend this course enough. I am now in the
-          job of my dreams and so excited about the future.{" "}
-        </p>
-      </div>
       <div className={styles.carruselDer}>
+        <Image className={styles.bg} src={bg} alt="bg" />
+        <Image className={styles.persona} src={tanya} alt="tanya" />
+      </div>
+      <div className={styles.carruselIzq}>
+        <Image className={styles.curve} src={curve} alt="curve" />
+        <Image className={styles.quotes} src={quotes} alt="quotes" />
         <p>
-          {" "}
           I amve been interested in coding for a while but never taken the jump,
           until now. I could not recommend this course enough. I am now in the
           job of my dreams and so excited about the future.{" "}
         </p>
+        <div>
+          <p> Tanya Sinclair </p>
+          <p> UX Engineer </p>
+        </div>
       </div>
     </div>
   );
