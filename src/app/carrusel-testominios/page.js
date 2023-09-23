e95e22606fc3b8aed6d7f3e1488cf8633733276e
catharygr <103951447+catharygr@users.noreply.github.com>
2023-09-23 11:55:02 +0100

    Trabajando con la página carrusel

---
+++ src/app/carrusel-testominios/page.js
@@ -0,0 +1,12 @@
+export default function Carrusel() {
+  return (
+    <div className="container">
+      <p>
+        {" "}
+        I amve been interested in coding for a while but never taken the jump,
+        until now. I could not recommend this course enough. I am now in the job
+        of my dreams and so excited about the future.{" "}
+      </p>
+    </div>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -4,9 +4,12 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
+    <main className={styles.main}>
       <Link className={styles.about} href="/producto-comercio-electronico">
         Producto de Comercio Electrónico
+      </Link>
+      <Link className={styles.about} href="/carrusel-testominios">
+        Carrusel de testimonios
       </Link>
     </main>
   );
