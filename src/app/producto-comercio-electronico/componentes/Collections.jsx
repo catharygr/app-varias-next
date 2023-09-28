eb7a97582a4dd894c347ba4fcf3534ea953c9e2d
catharygr <103951447+catharygr@users.noreply.github.com>
2023-09-28 12:21:15 +0100

    Trabajando con el componente Collections

--- src/app/producto-comercio-electronico/componentes/Collections.jsx
+++ src/app/producto-comercio-electronico/componentes/Collections.jsx
@@ -10,32 +10,55 @@ import product4 from "../image/image-product-4-thumbnail.jpg";
 import product4A from "../image/image-product-4.jpg";
 import { Minus, Plus } from "react-feather";
 
-const data = [
-  {
-    id: "1",
-    company: "Sneaker Company",
-    description:
-      "These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they'll withstand everything the weather can offer.",
-    name: "Fall Limited Edition Sneakers",
-    price: 125,
-    discont: 50,
-  },
-];
+const data = {
+  company: "Sneaker Company",
+  description:
+    "These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they'll withstand everything the weather can offer.",
+  name: "Fall Limited Edition Sneakers",
+  price: 250,
+  discont: 50,
+  images: [
+    {
+      id: "1",
+      urlGrande: product1A,
+      urlPequena: product1,
+      alt: "product1",
+    },
+    {
+      id: "2",
+      urlGrande: product2A,
+      urlPequena: product2,
+      alt: "product2",
+    },
+    {
+      id: "3",
+      urlGrande: product3A,
+      urlPequena: product3,
+      alt: "product3",
+    },
+    {
+      id: "4",
+      urlGrande: product4A,
+      urlPequena: product4,
+      alt: "product4",
+    },
+  ],
+};
 
 export default function Collection() {
-  const { id, company, name, description, price, discont } = data[0]; // Destructura los datos del producto
+  const { id, company, name, description, price, discont } = data[0];
 
   return (
     <div className={styles.container}>
       <div key={id} className={styles.producto}>
-        <Image src={product1A} alt="" />
-        <Image src={product1} alt="" />
-        <Image src={product2A} alt="" />
-        <Image src={product2} alt="" />
-        <Image src={product3A} alt="" />
-        <Image src={product3} alt="" />
-        <Image src={product4A} alt="" />
-        <Image src={product4} alt="" />
+        <Image src={product1A} alt={product1A} />
+        <Image src={product1} alt={product1} />
+        <Image src={product2A} alt={product2A} />
+        <Image src={product2} alt={product2} />
+        <Image src={product3A} alt={product3A} />
+        <Image src={product3} alt={product3} />
+        <Image src={product4A} alt={product4A} />
+        <Image src={product4} alt={product4} />
       </div>
       <div className={styles.contenido}>
         <h6>{company}</h6>
