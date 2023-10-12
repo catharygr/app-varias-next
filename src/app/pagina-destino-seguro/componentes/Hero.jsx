80a1b760e700f3f1cc290e5a52fb7de72d1198a1
catharygr <103951447+catharygr@users.noreply.github.com>
2023-10-12 15:32:47 +0100

    Trabajando con la media query

--- src/app/pagina-destino-seguro/componentes/Hero.jsx
+++ src/app/pagina-destino-seguro/componentes/Hero.jsx
@@ -52,7 +52,33 @@ export default function Hero() {
           />
         </div>
       </div>
-      <div className={styles.iconos}></div>
+      <div className={styles.iconos}>
+        <div>
+          <h2>We are different</h2>
+        </div>
+        <div>
+          <Image src={iconSnappyProcess} />
+          <h3>Snappy Process</h3>
+          <p>
+            Our application process can be completed in minutes, not hours.
+            Don't get stuck filling in tedious forms.
+          </p>
+
+          <Image src={affordablePrices} />
+          <h3>Affordable Prices</h3>
+          <p>
+            We don't want you worrying about high monthly costs. Our prices may
+            be low, but we still offer the best coverage possible.
+          </p>
+
+          <Image src={peopleFirst} />
+          <h3>People First</h3>
+          <p>
+            Our plans aren't full of conditions and clauses to prevent payouts.
+            We make sure you're covered when you need it.
+          </p>
+        </div>
+      </div>
     </>
   );
 }
