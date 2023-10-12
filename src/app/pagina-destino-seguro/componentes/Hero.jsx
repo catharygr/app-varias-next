bf194bcbdb19a85ae50170a59c04b3b6b236f2b6
catharygr <103951447+catharygr@users.noreply.github.com>
2023-10-12 14:27:30 +0100

    Trabajando con los icónos

--- src/app/pagina-destino-seguro/componentes/Hero.jsx
+++ src/app/pagina-destino-seguro/componentes/Hero.jsx
@@ -6,47 +6,63 @@ import bgPatternRightMobiles from "../image/bg-pattern-intro-right-mobile.svg";
 import bgPatternLeftMobiles from "../image/bg-pattern-intro-left-mobile.svg";
 import bgPatternLeftDesktop from "../image/bg-pattern-intro-left-desktop.svg";
 import bgPatternRightDesktop from "../image/bg-pattern-intro-right-desktop.svg";
+import iconSnappyProcess from "../image/icon-snappy-process.svg";
+import affordablePrices from "../image/icon-affordable-prices.svg";
 
 export default function Hero() {
   return (
-    <div className={styles.heroContainer}>
-      <div className={styles.picture}>
-        <picture>
-          <source
-            media="(max-width: .999rem)"
-            src={mobile}
-          />
-          <source
-            media="(min-width: 35rem)"
-            src={desktop}
-          />
-          <Image
-            src={mobile}
-            alt="Imagen de una familia"
-            priority={true}
-            className={styles.img}
-          />
-        </picture>
-      </div>
+    <>
+      <div className={styles.heroContainer}>
+        <div className={styles.picture}>
+          <picture>
+            <source
+              media="(max-width: .999rem)"
+              src={mobile}
+            />
+            <source
+              media="(min-width: 35rem)"
+              src={desktop}
+            />
+            <Image
+              src={mobile}
+              alt="Imagen de una familia"
+              priority={true}
+              className={styles.img}
+            />
+          </picture>
+        </div>
 
-      <div className={styles.contenidoTexto}>
-        <Image
-          src={bgPatternLeftMobiles}
-          className={styles.bgPatternLeftMobiles}
-        />
-        <div></div>
-        <h1 className={styles.h1}>Humanizing your insurance.</h1>
-        <p>
-          Get your life insurance coverage easier and faster. We blend our
-          expertise and technology to help you find the plan that's right for
-          you. Ensure you and your loved ones are protected.
-        </p>
-        <button className={styles.btn}>View Plans</button>
-        <Image
-          src={bgPatternRightMobiles}
-          className={styles.patternRight}
-        />
+        <div className={styles.contenidoTexto}>
+          <Image
+            src={bgPatternLeftMobiles}
+            className={styles.bgPatternLeftMobiles}
+          />
+          <div></div>
+          <h1 className={styles.h1}>Humanizing your insurance.</h1>
+          <p>
+            Get your life insurance coverage easier and faster. We blend our
+            expertise and technology to help you find the plan that's right for
+            you. Ensure you and your loved ones are protected.
+          </p>
+          <button className={styles.btn}>View Plans</button>
+          <Image
+            src={bgPatternRightMobiles}
+            className={styles.patternRight}
+          />
+        </div>
       </div>
-    </div>
+      <div>
+        <div>
+          <h2>We are different</h2>
+        </div>
+        <div>
+          <Image src={iconSnappyProcess} />
+
+          <Image src={affordablePrices} />
+
+          <Image src={iconSnappyProcess} />
+        </div>
+      </div>
+    </>
   );
 }
