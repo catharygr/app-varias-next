77ed4302d0b50b493f401a038f262b46729f4c05
catharygr <103951447+catharygr@users.noreply.github.com>
2023-10-13 17:27:22 +0100

    Terminado la app para mobile

--- src/app/pagina-destino-seguro/componentes/SubHeroDos.jsx
+++ src/app/pagina-destino-seguro/componentes/SubHeroDos.jsx
@@ -1,11 +1,24 @@
 import styles from "./SubHeroDos.module.css";
 import Image from "next/image";
+import howWeWorkDesktop from "../image/bg-pattern-how-we-work-desktop.svg";
+import howWeWorkMobile from "../image/bg-pattern-how-we-work-mobile.svg";
 
 export default function SubHeroDos() {
   return (
     <div className={styles.container}>
+      <Image
+        src={howWeWorkDesktop}
+        alt="how we work desktop"
+        className={styles.desktop}
+      />
+      <Image
+        src={howWeWorkMobile}
+        alt="how we work mobile"
+        className={styles.mobile}
+      />
+
       <h2 className={styles.h2}>Find out more about how we work</h2>
-      <button>How we work</button>
+      <button className={styles.btn}>How we work</button>
     </div>
   );
 }
