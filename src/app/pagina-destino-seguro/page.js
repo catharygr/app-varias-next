2dbdb2107c56ec215a0e3faec5c9e85e746c234e
catharygr <103951447+catharygr@users.noreply.github.com>
2023-10-13 15:59:29 +0100

    Trabajando en el componente SubHeroUno

--- src/app/pagina-destino-seguro/page.js
+++ src/app/pagina-destino-seguro/page.js
@@ -1,11 +1,13 @@
 import Header from "./componentes/Header/Header";
 import Hero from "./componentes/Hero";
+import SubHeroUno from "./componentes/SubHeroUno";
 
 export default function PaginaDestinoSeguro() {
   return (
     <div>
       <Header />
       <Hero />
+      <SubHeroUno />
     </div>
   );
 }
