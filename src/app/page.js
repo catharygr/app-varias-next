823688848526bb4e77179e516c8fb4f83698f484
catharygr <103951447+catharygr@users.noreply.github.com>
2023-10-16 14:26:21 +0100

    Prácticando flexbox

--- src/app/page.js
+++ src/app/page.js
@@ -5,14 +5,29 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <Link className={styles.about} href="/producto-comercio-electronico">
+      <Link
+        className={styles.about}
+        href="/producto-comercio-electronico"
+      >
         Producto de Comercio Electrónico
       </Link>
-      <Link className={styles.about} href="/carrusel-testominios">
+      <Link
+        className={styles.about}
+        href="/carrusel-testominios"
+      >
         Carrusel de testimonios
       </Link>
-      <Link className={styles.about} href="/pagina-destino-seguro">
+      <Link
+        className={styles.about}
+        href="/pagina-destino-seguro"
+      >
         Página de destino seguro
+      </Link>
+      <Link
+        className={styles.about}
+        href="/practica-flexbox"
+      >
+        Práctica Flex Box
       </Link>
     </main>
   );
---
+++ src/app/practica-flexbox/page.js
@@ -0,0 +1,27 @@
+import styles from "./flexBox.module.css";
+export default function FlexBox() {
+  return (
+    <>
+      <h2 className={styles.h2}>FlexBox</h2>
+      <div className={styles.flexContainer}>
+        <div>1</div>
+        <div>2</div>
+        <div className={styles.big}>3</div>
+        <div>4</div>
+        <div>5</div>
+        <div>6</div>
+        <div>7</div>
+        <div>8</div>
+      </div>
+      <h2 className={styles.h2}>Grid</h2>
+      <div className={styles.gridContainer}>
+        <div>1</div>
+        <div>2</div>
+        <div className={styles.big}>3</div>
+        <div>4</div>
+        <div>5</div>
+        <div>6</div>
+      </div>
+    </>
+  );
+}
