a65af69f3e84f9395e9ddc6c9ec1a5d5f502511f
catharygr <103951447+catharygr@users.noreply.github.com>
2023-10-19 17:18:50 +0100

    Trabajando con el componente SubHero

--- src/app/pagina-destino-seguro/componentes/SubHeroDos.jsx
+++ src/app/pagina-destino-seguro/componentes/SubHeroDos.jsx
@@ -5,6 +5,20 @@ import howWeWorkMobile from "../image/bg-pattern-how-we-work-mobile.svg";
 
 export default function SubHeroDos() {
   return (
-   
+    <div className={styles.container}>
+      <Image
+        src={howWeWorkDesktop}
+        alt="how we work desktop"
+        className={styles.desktop}
+      />
+      <Image
+        src={howWeWorkMobile}
+        alt="how we work mobile"
+        className={styles.mobile}
+      />
+
+      <h2 className={styles.h2}>Find out more about how we work</h2>
+      <button className={styles.btn}>How we work</button>
+    </div>
   );
 }
