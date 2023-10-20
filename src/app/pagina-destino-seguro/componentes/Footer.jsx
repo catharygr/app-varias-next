7a641e6f09b9c203e52b898d637a2ddc2674fa89
catharygr <103951447+catharygr@users.noreply.github.com>
2023-10-20 15:53:22 +0100

    Trabajando con el componente Footer y JSX

--- src/app/pagina-destino-seguro/componentes/Footer.jsx
+++ src/app/pagina-destino-seguro/componentes/Footer.jsx
@@ -7,5 +7,42 @@ import twitter from "../image/icon-twitter.svg";
 import Link from "next/link";
 
 export default function Footer() {
-  return <footer></footer>;
+  return (
+    <footer>
+      <div className={styles.container}>
+        <div className={styles.logo}>
+          <Image
+            src={logo}
+            alt="logo"
+          />
+        </div>
+        <div className={styles.social}>
+          <Link href="https://www.facebook.com/">
+            <Image
+              src={facebook}
+              alt="facebook"
+            />
+          </Link>
+          <Link href="https://www.twitter.com/">
+            <Image
+              src={twitter}
+              alt="twitter"
+            />
+          </Link>
+          <Link href="https://www.instagram.com/">
+            <Image
+              src={instagram}
+              alt="instagram"
+            />
+          </Link>
+          <Link href="https://www.instagram.com/">
+            <Image
+              src={instagram}
+              alt="instagram"
+            />
+          </Link>
+        </div>
+      </div>
+    </footer>
+  );
 }
