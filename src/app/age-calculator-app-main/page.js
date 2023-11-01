39192db28184721d7ee017238f29c49ea5a1bff3
catharygr <103951447+catharygr@users.noreply.github.com>
2023-11-01 14:14:57 +0000

    Trabajando en page

--- src/app/age-calculator-app-main/page.js
+++ src/app/age-calculator-app-main/page.js
@@ -64,7 +64,7 @@ export default function Home() {
           >
             - -
           </span>
-          anos
+          años
         </p>
         <p className={styles.text}>
           <span
