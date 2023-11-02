9aa52a440cd90ad5548c69d06ee8d65a948e7da3
catharygr <103951447+catharygr@users.noreply.github.com>
2023-11-02 16:24:04 +0000

    Página terminada

--- src/app/age-calculator-app-main/page.js
+++ src/app/age-calculator-app-main/page.js
@@ -1,6 +1,5 @@
 "use client";
-
-import React, { useState } from "react";
+import { useState } from "react";
 import styles from "./age.module.css";
 import Image from "next/image";
 import src from "./images/icon-arrow.svg";
@@ -10,198 +9,156 @@ export default function Home() {
   const [day, setDay] = useState("");
   const [month, setMonth] = useState("");
   const [year, setYear] = useState("");
+  const [years, setYears] = useState("--");
+  const [months, setMonths] = useState("--");
+  const [days, setDays] = useState("--");
   const [error, setError] = useState("");
 
-  const validateDate = (day, month, year) => {
-    // Validar que los campos no estén vacíos
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
     if (!day || !month || !year) {
-      return "Todos los campos son obligatorios";
+      setError("Todos los campos son obligatorios");
+      return;
     }
 
-    // Validar que el día esté entre 1 y 31
-    if (day < 1 || day > 31) {
-      return "El día debe estar entre 1 y 31";
-    }
+    const inputDay = parseInt(day);
+    const inputMonth = parseInt(month);
+    const inputYear = parseInt(year);
 
-    // Validar que el mes esté entre 1 y 12
-    if (month < 1 || month > 12) {
-      return "El mes debe estar entre 1 y 12";
-    }
-
-    // Validar que la fecha no sea en el futuro
-    const today = new Date();
-    const inputDate = new Date(year, month - 1, day);
-    if (inputDate > today) {
-      return "La fecha no puede ser en el futuro";
-    }
-
-    // Validar que la fecha sea válida
     if (
-      inputDate.getDate() !== Number(day) ||
-      inputDate.getMonth() + 1 !== Number(month) ||
-      inputDate.getFullYear() !== Number(year)
+      isNaN(inputDay) ||
+      isNaN(inputMonth) ||
+      isNaN(inputYear) ||
+      inputDay < 1 ||
+      inputDay > 31 ||
+      inputMonth < 1 ||
+      inputMonth > 12
     ) {
-      return "La fecha ingresada no es válida";
+      setError("Fecha no válida");
+      return;
     }
 
-    return "";
-  };
+    const currentDate = new Date();
+    const inputDate = new Date(inputYear, inputMonth - 1, inputDay);
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const errorMessage = validateDate(day, month, year);
-    if (errorMessage) {
-      setError(errorMessage);
-    } else {
-      setError("");
-      const calculateAge = (day, month, year) => {
-        const birthDate = new Date(year, month - 1, day);
-        const today = new Date();
-
-        let age = today.getFullYear() - birthDate.getFullYear();
-        const m = today.getMonth() - birthDate.getMonth();
-
-        if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-          age--;
-        }
-
-        return age;
-      };
+    if (inputDate > currentDate) {
+      setError("La fecha está en el futuro");
+      return;
     }
 
-    return (
-      <section className={styles.container}>
-        <form
-          className={styles.form}
-          onSubmit={handleSubmit}
-        >
-          {/* Tus campos de entrada aquí */}
-          <button
-            type="submit"
-            id="button"
-          >
-            <Image
-              src={src}
-              alt="Flecha hacia abajo que, al hacer clic, muestra la edad de la persona"
-              className={styles.iconArrow}
-            />
-          </button>
-        </form>
-        {error && <div className={styles.errorMessage}>{error}</div>}
-        {/* El resto de tu código de renderizado aquí */}
-      </section>
+    const ageInMilliseconds = currentDate - inputDate;
+    const ageInYears = Math.floor(
+      ageInMilliseconds / (365 * 24 * 60 * 60 * 1000)
     );
+    const ageInMonths = Math.floor(
+      (ageInMilliseconds / (30 * 24 * 60 * 60 * 1000)) % 12
+    );
+    const ageInDays = Math.floor(
+      (ageInMilliseconds / (24 * 60 * 60 * 1000)) % 30
+    );
+
+    setYears(ageInYears);
+    setMonths(ageInMonths);
+    setDays(ageInDays);
+
+    setError("");
   };
+
+  return (
+    <section className={styles.container}>
+      <form
+        className={styles.form}
+        onSubmit={handleSubmit}
+      >
+        <label htmlFor="day">
+          Día
+          <input
+            type="text"
+            id="day"
+            placeholder="DD"
+            required
+            value={day}
+            onChange={(e) => setDay(e.target.value)}
+          />
+          <p
+            className={styles.messageError}
+            id="messageError"
+          ></p>
+        </label>
+        <label htmlFor="month">
+          Mes
+          <input
+            type="text"
+            id="month"
+            placeholder="MM"
+            required
+            value={month}
+            onChange={(e) => setMonth(e.target.value)}
+          />
+          <p
+            className={styles.messageError}
+            id="messageError"
+          ></p>
+        </label>
+        <label htmlFor="year">
+          Año
+          <input
+            type="text"
+            id="year"
+            placeholder="YYYY"
+            required
+            value={year}
+            onChange={(e) => setYear(e.target.value)}
+          />
+          <p
+            className={styles.messageError}
+            id="messageError"
+          >
+            {error}
+          </p>
+        </label>
+        <button
+          type="submit"
+          id="button"
+        >
+          <Image
+            src={src}
+            alt="Flecha hacia abajo que, al hacer clic, muestra la edad de la persona"
+            className={styles.iconArrow}
+          />
+        </button>
+      </form>
+      <div className={styles.containerData}>
+        <p className={styles.text}>
+          <span
+            className={styles.data}
+            id="years"
+          >
+            {years}
+          </span>
+          años
+        </p>
+        <p className={styles.text}>
+          <span
+            className={styles.data}
+            id="months"
+          >
+            {months}
+          </span>
+          meses
+        </p>
+        <p className={styles.text}>
+          <span
+            className={styles.data}
+            id="days"
+          >
+            {days}
+          </span>
+          días
+        </p>
+        <div id="container-error"></div>
+      </div>
+    </section>
+  );
 }
-
-// import React, { useState } from "react";
-// import styles from "./age.module.css";
-// import Image from "next/image";
-// import src from "./images/icon-arrow.svg";
-// import "./styles.css";
-
-// export default function Home() {
-//   const [day, setDay] = useState("");
-//   const [month, setMonth] = useState("");
-//   const [year, setYear] = useState("");
-
-//   const handleSubmit = (event) => {
-//     event.preventDefault();
-//     // Here you can handle the form submission, for example, calculate the age based on the input
-//   };
-
-//   return (
-//     <section className={styles.container}>
-//       <form
-//         className={styles.form}
-//         onSubmit={handleSubmit}
-//       >
-//         <label htmlFor="day">
-//           Dia
-//           <input
-//             type="text"
-//             id="day"
-//             placeholder="DD"
-//             required
-//             value={day}
-//             onChange={(e) => setDay(e.target.value)}
-//           />
-//           <p
-//             className={styles.messageError}
-//             id="messageError"
-//           ></p>
-//         </label>
-//         <label htmlFor="month">
-//           Mes
-//           <input
-//             type="text"
-//             id="month"
-//             placeholder="MM"
-//             required
-//             value={month}
-//             onChange={(e) => setMonth(e.target.value)}
-//           />
-//           <p
-//             className={styles.messageError}
-//             id="messageError"
-//           ></p>
-//         </label>
-//         <label htmlFor="year">
-//           Año
-//           <input
-//             type="text"
-//             id="year"
-//             placeholder="YYYY"
-//             required
-//             value={year}
-//             onChange={(e) => setYear(e.target.value)}
-//           />
-//           <p
-//             className={styles.messageError}
-//             id="messageError"
-//           ></p>
-//         </label>
-//         <button
-//           type="submit"
-//           id="button"
-//         >
-//           <Image
-//             src={src}
-//             alt="Flecha hacia abajo que, al hacer clic, muestra la edad de la persona"
-//             className={styles.iconArrow}
-//           />
-//         </button>
-//       </form>
-//       <div className={styles.containerData}>
-//         <p className={styles.text}>
-//           <span
-//             className={styles.data}
-//             id="years"
-//           >
-//             {year}
-//           </span>
-//           años
-//         </p>
-//         <p className={styles.text}>
-//           <span
-//             className={styles.data}
-//             id="months"
-//           >
-//             {month}
-//           </span>
-//           meses
-//         </p>
-//         <p className={styles.text}>
-//           <span
-//             className={styles.data}
-//             id="days"
-//           >
-//             {day}
-//           </span>
-//           dias
-//         </p>
-//         <div id="container-error"></div>
-//       </div>
-//     </section>
-//   );
-// }
