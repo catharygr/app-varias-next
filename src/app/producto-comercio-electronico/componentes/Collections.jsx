93b8eab58ad2074ab21d148205577ea4dbdd7cd3
catharygr <103951447+catharygr@users.noreply.github.com>
2023-11-06 10:56:41 +0000

    Añadiendo el key para los elementos generados

--- src/app/producto-comercio-electronico/componentes/Collections.jsx
+++ src/app/producto-comercio-electronico/componentes/Collections.jsx
@@ -49,114 +49,83 @@ const data = {
 };
 
 export default function Collection() {
+  const [imagen, setImagen] = useState(data.images[0].urlGrande);
+  const { id, company, name, description, price, discont, images } = data;
+
+  function handleImageGrande() {
+    setImagen(images[0].urlGrande);
+  }
+
+  function handleImagePequena() {
+    setImagen(images[0].urlPequena);
+  }
+
   return (
-    <>
-      <div className="nav-right">
-        <Image
-          className="profile-image"
-          src="images/image-avatar.png"
-        />
-        <div className="modal hidden">
-          <div className="hidden-1">
-            <h4>Cart</h4>
-          </div>
-          <div className="cart_box hidden-2">
-            <div className="box-1">
-              <img
-                className="cart-box-image"
-                src="images/image-product-1-thumbnail.jpg"
-              />
-              <div>
-                <p className="box-1-p1">Fall Limited Edition Sneakers</p>
-                <div className="cart_price">
-                  <p className="cart_price_calc">$125.00 x </p>
-                  <p className="cart_price_count">1</p>
-                  <h5 className="cart_total_price">$250.00</h5>
-                </div>
-              </div>
-              <Image
-                className="delete"
-                src="images/delete.png"
-                alt="delete"
-              />
-            </div>
-            <div className="checkout">checkout</div>
-          </div>
-        </div>
-      </div>
-      <div className="cart_logo_count">0</div>
-
-      <div className="container">
-        <div className="container-1">
-          <Image
-            className="main-image"
-            src="images/image-product-1.jpg"
-          />
-          <ul className="bottom-images">
-            <li className="image-list">
-              <Image
-                className="bottom-img"
-                src="images/image-product-1-thumbnail.jpg"
-              />
-            </li>
-            <li className="image-list">
-              <Image
-                className="bottom-img"
-                src="images/image-product-2-thumbnail.jpg"
-              />
-            </li>
-            <li className="image-list">
-              <Image
-                className="bottom-img"
-                src="images/image-product-3-thumbnail.jpg"
-              />
-            </li>
-            <li>
-              <img
-                className="bottom-img"
-                src="images/image-product-4-thumbnail.jpg"
-              />
-            </li>
-          </ul>
-        </div>
-        <div className="container-2">
-          <h3 className="company">SNEAKER COMPANY</h3>
-          <h1 className="edition">Fall Limited Edition Sneakers</h1>
-          <p className="shoe-details">
-            These low-profile sneakers are your perfect casual wear companion.
-            Featuring a durable rubber outer sole, they’ll withstand everything
-            the weather can offer.
-          </p>
-          <div className="price">
-            <h3 className="disc-price">$125.00</h3>
-            <h4 className="offer">50%</h4>
-          </div>
-          <h4 className="real-price">$250.00</h4>
-          <div className="cart">
-            <div
-              className="cart-count"
-              width="12"
-              height="4"
+    <div className={styles.container}>
+      <div className={styles.producto}>
+        <div className={styles.imagenGrande}>
+          {images.map((image, id) => (
+            <button
+              key={image.id}
+              onClick={handleImageGrande}
+              className={styles.btnVolver}
             >
-              <h5 className="cart_count">0</h5>
-            </div>
-            <div className="add-cart">
-              <h4 className="add-to-cart">Add to cart</h4>
-            </div>
-          </div>
+              <ChevronLeft
+                className={styles.iconLeft}
+                size={30}
+              />
+              <Image
+                key={id}
+                src={image.urlGrande}
+                alt={image.alt}
+              />
+              <ChevronRight
+                className={styles.iconRight}
+                size={30}
+              />
+            </button>
+          ))}
+        </div>
+        <div className={styles.imagenPequena}>
+          {images.map((image, id) => (
+            <button
+              key={image.id}
+              onClick={handleImagePequena}
+              className={styles.btnVolver}
+            >
+              <Image
+                key={id}
+                src={image.urlPequena}
+                alt={image.alt}
+              />
+            </button>
+          ))}
         </div>
       </div>
+      <div className={styles.contenido}>
+        <h6>{company}</h6>
+        <h1>{name}</h1>
+        <h4>{description}</h4>
+        <p>
+          {`$${price}.00`}
+          <span>{`${discont}%`}</span>
+        </p>
 
-      <div className="attribution">
-        Challenge by
-        <a
-          href="https://www.frontendmentor.io?ref=challenge"
-          target="_blank"
-        >
-          Frontend Mentor
-        </a>
-        . Coded by <a href="#">Shadan Rashid</a>.
+        <div className={styles.cantidad}>
+          <button className={styles.btn}>
+            <Minus
+              className={styles.icons}
+              size={15}
+            />
+            0
+            <Plus
+              className={styles.icons}
+              size={15}
+            />
+          </button>
+          <button className={styles.btnCarrito}>Agregar al carrito</button>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
