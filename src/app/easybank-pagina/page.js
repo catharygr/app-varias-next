a5e148e42e803ac807d6942f1645f5b763c43eac
catharygr <103951447+catharygr@users.noreply.github.com>
2023-11-22 15:47:45 +0000

    Trabajando en la etiqueta Imagen y estilos

--- src/app/easybank-pagina/page.js
+++ src/app/easybank-pagina/page.js
@@ -1,11 +1,9 @@
 import Header from "./componentes/Header";
-import Articulos from "./componentes/Articulos";
 
 export default function EasybankPagina() {
   return (
     <div>
       <Header />
-      <Articulos />
     </div>
   );
 }
