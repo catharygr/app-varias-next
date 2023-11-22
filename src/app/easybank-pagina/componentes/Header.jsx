5f909cb0c338f439afcd50f87b3fa8f652be6af5
catharygr <103951447+catharygr@users.noreply.github.com>
2023-11-22 15:22:41 +0000

    Trabajando en el componente Header

--- src/app/easybank-pagina/componentes/Header.jsx
+++ src/app/easybank-pagina/componentes/Header.jsx
@@ -1,136 +1,50 @@
-"use client";
+import style from "./easybank.module.css";
 import Image from "next/image";
-import logo from "../image/logo.svg";
-import React from "react";
+import Logo from "./images/logo.svg";
+import ButtonMenuMobile from "../assets/images/buttonMenuMobile.svg";
+import ButtonCloseMenuMobile from "../assets/images/icon-close.svg";
+import NavegacionMobiles from "./NavegacionMobiles";
+import useState from "react";
 
-export default function Header() {
-  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
-  function toggleMenu() {
-    const openBtn = document.getElementById("openBtn");
-    const closeBtn = document.getElementById("closeBtn");
-    const isMenuOpen = document.getElementById("isMenuOpen");
-    if (isMenuOpen.classList.contains("hidden")) {
-      isMenuOpen.classList.remove("hidden");
-      isMenuOpen.classList.add("block");
-      openBtn.classList.add("hidden");
-      closeBtn.classList.remove("hidden");
-    } else {
-      isMenuOpen.classList.remove("block");
-      isMenuOpen.classList.add("hidden");
-      openBtn.classList.remove("hidden");
-      closeBtn.classList.add("hidden");
-    }
-  }
+function Header() {
+  const [openMenu, setOpenMenu] = useState(false);
   return (
-    <header className="bg-white p-6 relative">
-      <nav className="max-w-5xl mx-auto flex items-center justify-between">
-        <div className="">
-          <Image
-            src={logo}
-            alt="logo"
-            className="cursor-pointer"
-          />
-        </div>
-        <button
-          className="sm:hidden block"
-          id="openBtn"
-          onClick={toggleMenu}
-        >
-          {isMenuOpen ? (
-            <img
-              src={closeBtn}
-              alt="Close"
-            />
-          ) : (
-            <img
-              src={isMenuOpen}
-              alt="Menu"
-            />
-          )}
-        </button>
-
-        <div className="md:block hidden space-x-4">
-          <a
-            href="#"
-            className="text-lg font-semibold text-gray-500 hover:text-green-400 transition hover:delay-200 hover:duration-300 hover:ease-in"
-          >
-            Home
-          </a>
-          <a
-            href="#"
-            className="text-lg font-semibold text-gray-500 hover:text-green-400 transition hover:delay-200 hover:duration-300 hover:ease-in"
-          >
-            About
-          </a>
-          <a
-            href="#"
-            className="text-lg font-semibold text-gray-500 hover:text-green-400 transition hover:delay-200 hover:duration-300 hover:ease-in"
-          >
-            Contact
-          </a>
-          <a
-            href="#"
-            className="text-lg font-semibold text-gray-500 hover:text-green-400 transition hover:delay-200 hover:duration-300 hover:ease-in"
-          >
-            Blog
-          </a>
-          <a
-            href="#"
-            className="text-lg font-semibold text-gray-500 hover:text-green-400 transition hover:delay-200 hover:duration-300 hover:ease-in"
-          >
-            Careers
-          </a>
-        </div>
-
-        <div className="sm:block hidden mx-4">
-          <a
-            href="#"
-            className="px-6 py-3 text-white font-semibold bg-gradient-to-r from-green-400 to-cyan-400 rounded-full"
-          >
-            Request Invite
-          </a>
-        </div>
-      </nav>
-      <section
-        className={`max-w-5xl mx-auto absolute top-12 right-0 left-0 bg-black bg-opacity-30 min-h-screen ${
-          isMenuOpen ? "block" : "hidden"
-        }`}
-      >
-        <div className="max-w-xs my-8 rounded mx-auto bg-white h-64">
-          <div className="space-y-4 flex flex-col items-center py-6">
-            <a
-              href="#"
-              className="text-lg font-semibold text-gray-500 hover:text-green-400 transition hover:delay-200 hover:duration-300 hover:ease-in"
-            >
-              Home
-            </a>
-            <a
-              href="#"
-              className="text-lg font-semibold text-gray-500 hover:text-green-400 transition hover:delay-200 hover:duration-300 hover:ease-in"
-            >
-              About
-            </a>
-            <a
-              href="#"
-              className="text-lg font-semibold text-gray-500 hover:text-green-400 transition hover:delay-200 hover:duration-300 hover:ease-in"
-            >
-              Contact
-            </a>
-            <a
-              href="#"
-              className="text-lg font-semibold text-gray-500 hover:text-green-400 transition hover:delay-200 hover:duration-300 hover:ease-in"
-            >
-              Blog
-            </a>
-            <a
-              href="#"
-              className="text-lg font-semibold text-gray-500 hover:text-green-400 transition hover:delay-200 hover:duration-300 hover:ease-in"
-            >
-              Careers
-            </a>
-          </div>
-        </div>
-      </section>
-    </header>
+    <>
+      <header className={style.container}>
+        <Image
+          src={Logo}
+          alt="logo"
+          className={style.logo}
+        />
+        <ul className={style.list_desktop}>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+          <li>
+            <a href="#">Blog</a>
+          </li>
+          <li>
+            <a href="#">Careers</a>
+          </li>
+        </ul>
+        <button className={style.button_desktop}>Request Invite</button>
+        <img
+          src={openMenu ? ButtonCloseMenuMobile : ButtonMenuMobile}
+          className={
+            openMenu ? style.buttonMenuMobileClose : style.buttonMenuMobile
+          }
+          onClick={() => setOpenMenu(!openMenu)}
+        />
+      </header>
+      {openMenu && <NavegacionMobiles />}
+    </>
   );
 }
+
+export default Header;
---
+++ src/app/easybank-pagina/componentes/NavegacionMobiles.jsx
@@ -0,0 +1,25 @@
+function NavegacionMobiles() {
+  return (
+    <nav className={style.container}>
+      <ul className={style.list}>
+        <a href="#">
+          <li>Home</li>
+        </a>
+        <a href="#">
+          <li>About</li>
+        </a>
+        <a href="#">
+          <li>Contact</li>
+        </a>
+        <a href="#">
+          <li>Blog</li>
+        </a>
+        <a href="#">
+          <li>Careers</li>
+        </a>
+      </ul>
+    </nav>
+  );
+}
+
+export default NavigationMobile;
