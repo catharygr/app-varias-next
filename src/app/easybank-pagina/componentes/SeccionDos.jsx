c111541d91c9dc104af572ff51f5e880d7a887fb
catharygr <103951447+catharygr@users.noreply.github.com>
2023-11-23 17:44:32 +0000

    Trabajando en el componente SeccionDos y su sintaxis JSX

---
+++ src/app/easybank-pagina/componentes/SeccionDos.jsx
@@ -0,0 +1,76 @@
+import styles from "./SeccionDos.module.css";
+import online from "../images/icon-online.svg";
+import budgeting from "../images/icon-budgeting.svg";
+import onboarding from "../images/icon-onboarding.svg";
+import api from "../images/icon-api.svg";
+
+function SectionTwo() {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.titles}>Why choose Easybank?</h1>
+
+      <p className={styles.paragraph}>
+        We leverage Open Banking to turn your bank account into your financial
+        hub. Control your finances like never before.
+      </p>
+
+      <div className={styles.container_1}>
+        <div className={styles.card}>
+          <img
+            src={online}
+            className={styles.imgs}
+          />
+          <h2 className={styles.subtitle}>Online Banking</h2>
+
+          <p className={styles.paragraph}>
+            Our modern web and mobile applications allow you to keep track of
+            your finances wherever you are in the world.
+          </p>
+        </div>
+
+        <div className={styles.card}>
+          <img
+            src={budgeting}
+            className={styles.imgs}
+          />
+
+          <h2 className={styles.subtitle}>Simple Budgeting</h2>
+
+          <p className={styles.paragraph}>
+            See exactly where your money goes each month. Receive notifications
+            when you’re close to hitting your limits.
+          </p>
+        </div>
+
+        <div className={styles.card}>
+          <img
+            src={onboarding}
+            className={styles.imgs}
+          />
+          <h2 className={styles.subtitle}>Fast Onboarding</h2>
+
+          <p className={styles.paragraph}>
+            We don’t do branches. Open your account in minutes online and start
+            taking control of your finances right away.
+          </p>
+        </div>
+
+        <div className={styles.card}>
+          <img
+            src={api}
+            className={styles.imgs}
+          />
+
+          <h2 className={styles.subtitle}>Open API</h2>
+
+          <p className={styles.paragraph}>
+            Manage your savings, investments, pension, and much more from one
+            account. Tracking your money has never been easier.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SectionTwo;
--- src/app/easybank-pagina/page.js
+++ src/app/easybank-pagina/page.js
@@ -1,11 +1,13 @@
 import Header from "./componentes/Header";
 import SeccionUna from "./componentes/SeccionUna";
+import SeccionDos from "./componentes/SeccionDos";
 
 export default function EasybankPagina() {
   return (
     <div>
       <Header />
       <SeccionUna />
+      <SeccionDos />
     </div>
   );
 }
