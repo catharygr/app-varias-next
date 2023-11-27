2312046ba0238d8cd6c0bf2d5aebcba4049e34a9
catharygr <103951447+catharygr@users.noreply.github.com>
2023-11-27 19:41:41 +0000

    Trabajando en JSX

--- src/app/easybank-pagina/componentes/NavegacionMobiles.jsx
+++ src/app/easybank-pagina/componentes/NavegacionMobiles.jsx
@@ -2,7 +2,25 @@ import styles from "./NavegacionMobiles.module.css";
 
 function NavegacionMobiles() {
   return (
-
+    <nav className={styles.container}>
+      <ul className={styles.list}>
+        <a href="#">
+          <li>Home</li>
+        </a>
+        <a href="#">
+          <li>About</li>
+        </a>
+        <a href="#">
+          <li>Contact</li>
+        </a>
+        <a href="#">
+          <li>Blog</li>
+        </a>
+        <a href="#">
+          <li>Careers</li>
+        </a>
+      </ul>
+    </nav>
   );
 }
 
